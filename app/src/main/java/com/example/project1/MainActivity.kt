4edb9c3bd032940dec19e701edd61f23ab28f36a
toto-lacky <totolacky@kaist.ac.kr>
2019-12-27 14:41:37 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.project1
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.project1.ui.main.SectionsPagerAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.example.project1.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
+
+//import android.support.v4.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter by lazy{MainAdapter(supportFragmentManager)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +36,64 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        view_pager.adapter = MainActivity@adapter
     }
+}
+
+class AFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //뷰 설정
+        section_label.text = "A Fragment"
+
+    }
+
+}
+
+class BFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //뷰 설정
+        section_label.text = "B Fragment"
+
+    }
+
+}
+
+class MainAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment? {
+
+        return when(position) {
+
+            0       ->  AFragment()
+
+            1       ->  BFragment()
+
+            else    ->  null
+        }
+
+    }
+
+    // 생성 할 Fragment 의 개수
+    override fun getCount() = 2
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        super.destroyItem(container, position, `object`)
+        //Log.e("FragmentPagerAdapter", "destroyItem position : $position")
+    }
+
 }
