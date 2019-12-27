f4ac7c8a04e6bd8c7199c2a78aab13a94e92120a
toto-lacky <totolacky@kaist.ac.kr>
2019-12-27 16:09:37 +0900

    Addr 탭까지 탭 구조 완성

--- app/src/main/java/com/example/project1/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/project1/ui/main/SectionsPagerAdapter.kt
@@ -8,7 +8,8 @@ import com.example.project1.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -21,7 +22,10 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return when(position) {
+            0       ->  AddressFragment.newInstance(position + 1)
+            else    ->  PlaceholderFragment.newInstance(position + 1)
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -29,7 +33,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        // Show 3 total pages.
+        return 3
     }
 }
