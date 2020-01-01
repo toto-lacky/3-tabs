ab3bf85833a82b92c173520f97e90647793b9a0a
Hangil Lim <lhg0993@naver.com>
2020-01-01 18:35:24 +0900

    Update GalleryFragment.java
    데이터 저장 해보려고 노력함 다 주석처리해놈

--- app/src/main/java/com/example/project1_java/ui/main/GalleryFragment.java
+++ app/src/main/java/com/example/project1_java/ui/main/GalleryFragment.java
@@ -1,12 +1,15 @@
 package com.example.project1_java.ui.main;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,6 +54,22 @@ public class GalleryFragment extends Fragment {
             imageview[i].setOnClickListener(new ImageClickListener(i));
             imageview[i].setOnLongClickListener(new ImageLongClickListener(i));
         }
+/*
+        SharedPreferences sf = getActivity().getSharedPreferences("sFile", Context.MODE_PRIVATE);
+        
+        for(int i = 0; i<24; i++){
+            String uriID = "uri" +i;
+            uris_s[i] = sf.getString(uriID,"");
+            if(uris_s[i].equals("")) continue;
+            uris[i] = Uri.parse(uris_s[i]);
+            imageview[i].setImageURI(uris[i]);
+        }
+
+        imageview[0].setImageURI(Uri.parse(sf.getString("1234","")));
+        Log.d("error_log",""+Uri.parse(sf.getString("1234","")));
+        Log.d("error log",sf.getString("1234",""));
+
+         */
         return root;
     }
 
@@ -67,7 +86,7 @@ public class GalleryFragment extends Fragment {
         }
     }
 
-    public class ImageLongClickListener implements  View.OnLongClickListener{
+    public class ImageLongClickListener implements View.OnLongClickListener{
         int index;
         public ImageLongClickListener(int index){
             this.index = index;
@@ -86,6 +105,29 @@ public class GalleryFragment extends Fragment {
             uris[requestCode] = data.getData();
             imageview[requestCode].setImageURI(uris[requestCode]);
             uris_s[requestCode] = uris[requestCode].toString();
+            Log.d("error log",uris_s[requestCode]);
         }
     }
+/*
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        SharedPreferences sf = getActivity().getSharedPreferences("sFile", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sf.edit();
+        editor.putString("1234", uris_s[0]);
+
+        for(int i = 0; i<24; i++){
+            String uriID = "uri" + i;
+            editor.putString(uriID, uris_s[i]);
+        }
+
+        if(uris_s[0] != null) {
+            Log.d("error log", uris_s[0]);
+        }
+        editor.commit();
+    }
+
+ */
 }
