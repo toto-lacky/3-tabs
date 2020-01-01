8d8f1b0d7ffefec81f112cecd5f31456a8f12c2a
toto-lacky <totolacky@kaist.ac.kr>
2020-01-01 18:23:30 +0900

    fix: Loading page always shows up
    - setPermission 함수 단순화
    - loading 화면 항상 뜨도록 수정

--- app/src/main/java/com/example/project1_java/LoadingActivity.java
+++ app/src/main/java/com/example/project1_java/LoadingActivity.java
@@ -20,55 +20,36 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
 
         String[] reqPermissions = {Manifest.permission.READ_CONTACTS, Manifest.permission.CALL_PHONE};
-        int neededPermissions = setPermission(this, reqPermissions);
-
-        if(neededPermissions == 0)
-            switchActivity();
+        ActivityCompat.requestPermissions(this, reqPermissions,0);
     }
 
     /* 권한 확인 및 요청 함수 */
-    public static int setPermission(Context context, String[] permissions){
-        /* 추가적으로 요청해야 하는 권한들 */
-        ArrayList<String> ReqPerm = new ArrayList<>();
-
-        /* 현재 권한이 있는지 확인, 없으면 ReqPerm에 추가 */
-        for (String perm : permissions) {
-            if(ContextCompat.checkSelfPermission(context, perm) == PackageManager.PERMISSION_GRANTED);
-                //Log.d("Permission","Already have permission: " + perm);
-            else
-                ReqPerm.add(perm);
-        }
-
-        /* 권한 요청 */
-        if(ReqPerm.size() != 0)
-            ActivityCompat.requestPermissions((Activity) context, ReqPerm.toArray(new String[0]),0);
-
-        return ReqPerm.size();
+    public static void setPermission(Context context, String[] permissions){
+        ActivityCompat.requestPermissions((Activity) context, permissions,0);
     }
 
     /* 권한을 모두 획득하면 appActivity 실행, 그렇지 않으면 다시 권한 요청 */
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
-        Log.d("ReqPermRes","requestCode = "+requestCode);
-        Log.d("ReqPermRes","permissions: "+permissions);
-        Log.d("ReqPermRes","grantResults: "+grantResults);
+//        Log.d("ReqPermRes","requestCode = "+requestCode);
+//        Log.d("ReqPermRes","permissions: "+permissions);
+//        Log.d("ReqPermRes","grantResults: "+grantResults);
 
         /* 다시 요청해야 하는 권한들 */
-        ArrayList<String> ReqPerm = new ArrayList<>();
+        int cnt = 0;
         for (int i = 0; i < permissions.length; i++) {
             if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
                 Log.d("Permission","permission " + permissions[i] + " granted");
             } else {
-                ReqPerm.add(permissions[i]);
+                cnt++;
                 Log.d("Permission","permission " + permissions[i] + " denied");
             }
         }
 
-        if(ReqPerm.size() > 0)
-            ActivityCompat.requestPermissions(this, ReqPerm.toArray(new String[0]),0);
-
-        if(ReqPerm.size() == 0){
+        if(cnt > 0) {
+            ActivityCompat.requestPermissions(this, permissions, 0);
+        } else {
             switchActivity();
         }
     }
